99559d1481eaf1b03aff2b7828969f83a4c145c2
sarisma123 <sarishmaghimire45@gmail.com>
2026-09-21 13:16:32 +0545

    replaced the proxy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-
   server: {
     port: 5173,
     proxy: {
@@ -11,7 +10,7 @@ export default defineConfig({
         target: "http://localhost:8081",
         changeOrigin: true,
         rewrite: (path) =>
-          path.replace(/^\/api/, "/Student_Mart/backend/api"),
+          path.replace(/^\/api/, "/Student_Mart/backend/pages"),
       },
     },
   },
